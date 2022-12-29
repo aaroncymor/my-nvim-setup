90f2c52366e14bf847460e11943ea1b678e6739e
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2022-12-29 21:49:19 +0800

    Tabstop and shiftwidth to 2 instead of 4

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -3,10 +3,13 @@ vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.opt.smartindent = true
 
