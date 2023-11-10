eeca073e0ea2ed262a599a0f09d5896788b42285
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2023-11-10 19:52:01 +0800

    Adding packer commands notes

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 require("custom")
 
+-- Packer Commands
+-- :PackerInstall
+-- :PackerSync
+-- :PackerUpdate
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
